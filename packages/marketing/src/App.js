d7421cc2211fa14cff11fafde70d766cbc4e6c53
Pavel Kruglik <stasdev001@gmail.com>
2022-04-09 13:09:46 +0300

    added styles prefixes

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassname = createGenerateClassName({ productionPrefix: 'ma' });
+
 const App = () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassname}>
         <Router>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
